8b05f05ed3bb2f254c6e720b08d8120b870342c4
kyle <kyle@solidstategroup.com>
2018-08-25 13:52:02 +0100

    site design

--- web/main.js
+++ web/main.js
@@ -1,1 +1,21 @@
 import './styles/styles.scss';
+
+const navClass = 'nav__past-hero';
+const NAV_OFFSET = 20;
+jQuery(function ($) {
+
+    var $nav = $('.nav');
+    var $win = $(window);
+    var winH = $win.height() - NAV_OFFSET;   // Get the window height.
+
+    $win.on("scroll", function () {
+        if ($(this).scrollTop() > winH) {
+            $nav.addClass(navClass);
+        } else {
+            $nav.removeClass(navClass);
+        }
+    }).on("resize", function () { // If the user resizes the window
+        winH = $(this).height() - NAV_OFFSET; // you'll need the new height value
+    });
+
+});
