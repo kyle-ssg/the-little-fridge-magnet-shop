8935b5e3a9521ce8a03271a559d16eaf29cf1545
kyle <kyle@solidstategroup.com>
2018-08-27 15:27:09 +0100

    Nav scroll fix

--- web/scripts/lfm-nav-scroll.js
+++ web/scripts/lfm-nav-scroll.js
@@ -5,7 +5,7 @@ jQuery(function ($) {
 
     var $nav = $('.nav');
     var $win = $(window);
-    var winH = $win.height() - NAV_OFFSET;   // Get the window height.
+    var winH = document.body.clientWidth > 768 ? $win.height() - NAV_OFFSET : $win.height() / 4 - NAV_OFFSET;   // Get the window height.
 
     $win.on("scroll", function () {
         if ($(this).scrollTop() > winH) {
@@ -14,7 +14,7 @@ jQuery(function ($) {
             $nav.removeClass(navClass);
         }
     }).on("resize", function () { // If the user resizes the window
-        winH = $(this).height() - NAV_OFFSET; // you'll need the new height value
+        winH = document.body.clientWidth > 768 ? $win.height() - NAV_OFFSET : $win.height() / 4 - NAV_OFFSET;   // Get the window height.
     });
 
 });
