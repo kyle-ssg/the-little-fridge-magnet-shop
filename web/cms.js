19a420dc66a11d2a655d629033873eb8650daae8
Jon Sherwood <jsherwood@dmpro.co.uk>
2018-08-28 21:22:05 +0100

    Content updates

--- web/cms.js
+++ web/cms.js
@@ -2,177 +2,11 @@ module.exports = {
     collections: [
         {
             mainImage: {
-                uri: "/static/images/c1/1.jpg",
-                title: "Lorem Ipsum",
+                uri: "/static/images/c3/1b.jpg",
+                title: "3.00",
                 description: "",
-                width: "10cm",
-                height: "10cm",
-            },
-            grid : ()=>{
-                return {
-                    columnWidth: 200,
-                }
-            },
-            images: [
-                {
-                    uri: "/static/images/c1/2.jpg",
-                    title: "Lorem Ipsum",
-                    description: "",
-                    width: "10cm",
-                    height: "10cm",
-                },
-                {
-                    uri: "/static/images/c1/3.jpg",
-                    title: "Lorem Ipsum",
-                    description: "",
-                    width: "10cm",
-                    height: "10cm",
-                },
-                {
-                    uri: "/static/images/c1/4.jpg",
-                    title: "Lorem Ipsum",
-                    description: "",
-                    width: "10cm",
-                    height: "10cm",
-                },
-                {
-                    uri: "/static/images/c1/5.jpg",
-                    title: "Lorem Ipsum",
-                    description: "",
-                    width: "10cm",
-                    height: "10cm",
-                },
-                {
-                    uri: "/static/images/c1/6.jpg",
-                    title: "Lorem Ipsum",
-                    description: "",
-                    width: "10cm",
-                    height: "10cm",
-                },
-            ]
-        },
-        {
-            price: "",
-            mainImage: {
-                uri: "/static/images/c2/1.jpg",
-                title: "Lorem Ipsum",
-                description: "",
-                width: "10cm",
-                height: "10cm",
-            },
-            grid : ()=>{
-                return {
-                    columnWidth: 200,
-                }
-            },
-            images: [
-                {
-                    uri: "/static/images/c2/2.jpg",
-                    title: "Lorem Ipsum",
-                    description: "",
-                    width: "10cm",
-                    height: "10cm",
-                },
-                {
-                    uri: "/static/images/c2/3.jpg",
-                    title: "Lorem Ipsum",
-                    description: "",
-                    width: "10cm",
-                    height: "10cm",
-                },
-                {
-                    uri: "/static/images/c2/4.jpg",
-                    title: "Lorem Ipsum",
-                    description: "",
-                    width: "10cm",
-                    height: "10cm",
-                },
-                {
-                    uri: "/static/images/c2/5.jpg",
-                    title: "Lorem Ipsum",
-                    description: "",
-                    width: "10cm",
-                    height: "10cm",
-                },
-                {
-                    uri: "/static/images/c2/6.jpg",
-                    title: "Lorem Ipsum",
-                    description: "",
-                    width: "10cm",
-                    height: "10cm",
-                },
-                {
-                    uri: "/static/images/c2/7.jpg",
-                    title: "Lorem Ipsum",
-                    description: "",
-                    width: "10cm",
-                    height: "10cm",
-                },
-                {
-                    uri: "/static/images/c2/8.jpg",
-                    title: "Lorem Ipsum",
-                    description: "",
-                    width: "10cm",
-                    height: "10cm",
-                },
-                {
-                    uri: "/static/images/c2/9.jpg",
-                    title: "Lorem Ipsum",
-                    description: "",
-                    width: "10cm",
-                    height: "10cm",
-                },
-                {
-                    uri: "/static/images/c2/10.jpg",
-                    title: "Lorem Ipsum",
-                    description: "",
-                    width: "10cm",
-                    height: "10cm",
-                },
-                {
-                    uri: "/static/images/c2/11.jpg",
-                    title: "Lorem Ipsum",
-                    description: "",
-                    width: "10cm",
-                    height: "10cm",
-                },
-                {
-                    uri: "/static/images/c2/12.jpg",
-                    title: "Lorem Ipsum",
-                    description: "",
-                    width: "10cm",
-                    height: "10cm",
-                },
-                {
-                    uri: "/static/images/c2/13.jpg",
-                    title: "Lorem Ipsum",
-                    description: "",
-                    width: "10cm",
-                    height: "10cm",
-                },
-                {
-                    uri: "/static/images/c2/14.jpg",
-                    title: "Lorem Ipsum",
-                    description: "",
-                    width: "10cm",
-                    height: "10cm",
-                },
-                {
-                    uri: "/static/images/c2/15.jpg",
-                    title: "Lorem Ipsum",
-                    description: "",
-                    width: "10cm",
-                    height: "10cm",
-                },
-            ]
-        },
-        {
-            mainImage: {
-                uri: "/static/images/c3/1.jpg",
-                title: "Lorem Ipsum",
-                description: "",
-                width: "10cm",
-                height: "10cm",
+                width: "3cm",
+                height: "4cm",
             },
             grid : ()=>{
                 return {
@@ -184,50 +18,181 @@ module.exports = {
                     uri: "/static/images/c3/2.jpg",
                     title: "Lorem Ipsum",
                     description: "",
-                    width: "10cm",
-                    height: "10cm",
+                    width: "8cm",
+                    height: "8cm",
                 },
                 {
                     uri: "/static/images/c3/3.jpg",
                     title: "Lorem Ipsum",
                     description: "",
-                    width: "10cm",
-                    height: "10cm",
+                    width: "8cm",
+                    height: "8cm",
                 },
                 {
                     uri: "/static/images/c3/4.jpg",
                     title: "Lorem Ipsum",
                     description: "",
-                    width: "10cm",
-                    height: "10cm",
+                    width: "8cm",
+                    height: "8cm",
                 },
                 {
                     uri: "/static/images/c3/5.jpg",
                     title: "Lorem Ipsum",
                     description: "",
-                    width: "10cm",
-                    height: "10cm",
-                },
-                {
-                    uri: "/static/images/c3/6.jpg",
-                    title: "Lorem Ipsum",
-                    description: "",
-                    width: "10cm",
-                    height: "10cm",
+                    width: "8cm",
+                    height: "8cm",
                 },
                 {
                     uri: "/static/images/c3/7.jpg",
                     title: "Lorem Ipsum",
                     description: "",
-                    width: "10cm",
-                    height: "10cm",
+                    width: "8cm",
+                    height: "8cm",
                 },
                 {
                     uri: "/static/images/c3/8.jpg",
                     title: "Lorem Ipsum",
                     description: "",
-                    width: "10cm",
-                    height: "10cm",
+                    width: "8cm",
+                    height: "8cm",
+                },
+            ]
+        },
+        {
+            price: "",
+            mainImage: {
+                uri: "/static/images/c2/12.jpg",
+                title: "4.00",
+                description: "",
+                width: "5cm",
+                height: "6cm",
+            },
+            grid : ()=>{
+                return {
+                    columnWidth: 200,
+                }
+            },
+            images: [
+                {
+                    uri: "/static/images/c2/2.jpg",
+                    title: "Lorem Ipsum",
+                    description: "",
+                    width: "6cm",
+                    height: "6cm",
+                },
+                {
+                    uri: "/static/images/c2/3.jpg",
+                    title: "Lorem Ipsum",
+                    description: "",
+                    width: "6cm",
+                    height: "6cm",
+                },
+                {
+                    uri: "/static/images/c2/4.jpg",
+                    title: "Lorem Ipsum",
+                    description: "",
+                    width: "6cm",
+                    height: "6cm",
+                },
+                {
+                    uri: "/static/images/c2/9.jpg",
+                    title: "Lorem Ipsum",
+                    description: "",
+                    width: "6cm",
+                    height: "6cm",
+                },
+                {
+                    uri: "/static/images/c2/10.jpg",
+                    title: "Lorem Ipsum",
+                    description: "",
+                    width: "6cm",
+                    height: "6cm",
+                },
+                {
+                    uri: "/static/images/c2/11.jpg",
+                    title: "Lorem Ipsum",
+                    description: "",
+                    width: "6cm",
+                    height: "6cm",
+                },
+                {
+                    uri: "/static/images/c2/1.jpg",
+                    title: "Lorem Ipsum",
+                    description: "",
+                    width: "6cm",
+                    height: "6cm",
+                },
+                {
+                    uri: "/static/images/c2/13.jpg",
+                    title: "Lorem Ipsum",
+                    description: "",
+                    width: "6cm",
+                    height: "6cm",
+                },
+                {
+                    uri: "/static/images/c2/14.jpg",
+                    title: "Lorem Ipsum",
+                    description: "",
+                    width: "6cm",
+                    height: "6cm",
+                },
+                {
+                    uri: "/static/images/c2/15.jpg",
+                    title: "Lorem Ipsum",
+                    description: "",
+                    width: "6cm",
+                    height: "6cm",
+                },
+            ]
+        },
+        {
+            mainImage: {
+                uri: "/static/images/c1/4.jpg",
+                title: "5.00",
+                description: "",
+                width: "6cm",
+                height: "8cm",
+            },
+            grid : ()=>{
+                return {
+                    columnWidth: 200,
+                }
+            },
+            images: [
+                {
+                    uri: "/static/images/c1/2.jpg",
+                    title: "Lorem Ipsum",
+                    description: "",
+                    width: "4cm",
+                    height: "4cm",
+                },
+                {
+                    uri: "/static/images/c1/3.jpg",
+                    title: "Lorem Ipsum",
+                    description: "",
+                    width: "4cm",
+                    height: "4cm",
+                },
+                {
+                    uri: "/static/images/c1/1.jpg",
+                    title: "Lorem Ipsum",
+                    description: "",
+                    width: "4cm",
+                    height: "4cm",
+                },
+                {
+                    uri: "/static/images/c1/5.jpg",
+                    title: "Lorem Ipsum",
+                    description: "",
+                    width: "4cm",
+                    height: "4cm",
+                },
+                {
+                    uri: "/static/images/c1/6.jpg",
+                    title: "Lorem Ipsum",
+                    description: "",
+                    width: "4cm",
+                    height: "4cm",
                 },
             ]
         },
--- web/templates/image-description-template.js
+++ web/templates/image-description-template.js
@@ -1,13 +1,13 @@
 module.exports = (mainImage) => (`
     <div>
         <h3 class="product__title">
-           Featured Item: <strong>${mainImage.title}</strong>
+           Price: £<strong>${mainImage.title}</strong> each  (inc. Postage & Packing)
         </h3>
         <p class="product__description">
             ${mainImage.description}
         </p>
         <p class="product__stats">
-            Height: ${mainImage.width} Width: ${mainImage.height}
+            Size: ${mainImage.width} to ${mainImage.height} (approx.)
         </p>
     </div>
 `);
